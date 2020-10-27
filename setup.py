5f3af5aef07d515d4a9811f15ba35040e69b58ac
duzive <duzive30@gmail.com>
2020-10-27 20:05:40 +0300

    Самое важное - версия!

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup
 :copyright: (c) 2020-2021 Duzive
 """
 
-version = '1.0.0'
+version = '1.0.1'
 
 with open('README.md', encoding='utf-8') as f:
     long_description = f.read()
--- worldcoin_wrapper/init.py
+++ worldcoin_wrapper/init.py
@@ -3,4 +3,4 @@ from .worldcoin import WorldCoin, LongPolling
 name = 'worldcoin_wrapper'
 
 __author__ = 'Duzive'
-__version__ = '1.0.0'
+__version__ = '1.0.1'
